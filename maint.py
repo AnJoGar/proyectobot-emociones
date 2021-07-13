1f635a8f6c86310ed5212a1b861dad10bd7f281d
Anderson2020 <andjor2020@gmail.com>
2021-07-13 02:35:33 -0500

    primer commit

---
+++ maint.py
@@ -0,0 +1,30 @@
+import telebot
+import subprocess
+
+from test import main
+
+
+bot = telebot.TeleBot('1811047199:AAHRjSQgDNAIErnnNFgBpqN9rXaB6Hp0NQU')
+
+
+@bot.message_handler(content_types=['voice'])
+def handel(message):
+    fileID = message.voice.file_id
+    file = bot.get_file(fileID)
+    down_file = bot.download_file(file.file_path)
+    with open('test.ogg', 'wb') as f:
+        f.write(down_file)
+
+    src_filename = 'test.ogg'
+    dest_filename = 'test.wav'
+
+    process = subprocess.run(
+        ['C:\\webem\\bin\\ffmpeg.exe', '-i', src_filename, dest_filename, '-y'])
+    if process.returncode != 0:
+        raise Exception("Error")
+
+    result= main()
+    bot.send_message(message.chat.id, result)
+
+
+bot.polling(none_stop=True)
